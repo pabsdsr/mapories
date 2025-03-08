e2441b8922cb6bd19352750a51c4ab630218bec0
Pablo <pds@ucsb.edu>
2025-03-08 10:38:12 -0800

    carousel for home page

--- app/app/home/page.tsx
+++ app/app/home/page.tsx
@@ -1,13 +1,16 @@
 import { AuthenticationForm } from '../../components/Authentication/Authentication';
 import { Welcome } from '../../components/Welcome/Welcome';
 import { AppStructure } from '../../components/AppShell/AppShell';
+import { CarouselCard } from '@/components/Carousel/CarouselCard';
+import { CarouselContainer } from '@/components/Carousel/CarouselContainer';
 
 
 export default function HomePage() {
   return (
     <>
       <AppStructure>
-        <Welcome></Welcome>
+        {/* <CarouselCard></CarouselCard> */}
+        <CarouselContainer></CarouselContainer>
       </AppStructure>
 
     </>
